806402d3775b94b3163c92889f565f737bf66b0a
Aloísio Carvalho <loistc.carvalho@gmail.com>
2022-05-31 20:38:11 -0300

    iniciando repositorio

---
+++ gateway/src/main/java/com/dh/gateway/config/LogFilter.java
@@ -0,0 +1,32 @@
+package com.dh.gateway.config;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.Calendar;
+import java.util.logging.Logger;
+
+@Component
+public class LogFilter extends AbstractGatewayFilterFactory<LogFilter.Config> {
+
+    private static final Logger logger = Logger.getLogger(LogFilter.class.getName());
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            logger.info("Path request: " + exchange.getRequest().getPath());
+
+            return chain.filter(exchange).then(
+                Mono.fromRunnable(() -> {
+                    logger.info("Time response: " + Calendar.getInstance().getTime());
+                })
+            );
+        };
+    }
+
+    public  static class Config {
+        // config props
+    }
+}
